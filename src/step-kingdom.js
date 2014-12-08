29eff9ea7e8d5b2f7a2920144039a42d552dfe6e
devion-idle <devion.idle@gmail.com>
2014-12-07 21:21:54 -0500

    REST Get from Fitbit

--- src/step-kingdom.js
+++ src/step-kingdom.js
@@ -1,9 +1,33 @@
-console.log(OAuth.getVersion());
+var dailySteps = 0;
+
+// Get today's date in YYYY-MM-DD format
+function today() {
+    var date = new Date();
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1;
+    var yyyy = date.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+
+    return yyyy + '-' + mm + '-' + dd;
+}
+
 OAuth.initialize('R_b1DUSdIvkFS5Go8Zp3caHxOmE');
 OAuth.popup('fitbit')
-  .done(function(result) {
-    console.log(result)
-  })
-  .fail(function(err) {
-    console.log("Failed")
-  });
+    .done(function(result) {
+        console.log("Successfully connected to Fitbit.");
+        result.get("/1/user/-/activities/date/" + today() + ".xml")
+            .done(function(response)  {
+                dailySteps = response;
+                console.log(dailySteps);
+            });
+    })
+    .fail(function(err) {
+        console.log("Failed to connect to Fitbit.");
+    });
