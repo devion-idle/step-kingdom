606a627cd88ee5598719cc7e49e421aaa04c65cf
devion-idle <devion.idle@gmail.com>
2014-12-07 20:26:59 -0500

    Strip meteor code

--- src/step-kingdom.js
+++ src/step-kingdom.js
@@ -1,66 +1,9 @@
-// simple-todo.js
-
-Tasks = new Mongo.Collection("tasks");
-
-if (Meteor.isClient) {
-  OAuth.initialize('R_b1DUSdIvkFS5Go8Zp3caHxOmE')
-  OAuth.popup('fitbit')
+console.log(OAuth.getVersion());
+OAuth.initialize('R_b1DUSdIvkFS5Go8Zp3caHxOmE');
+OAuth.popup('fitbit')
   .done(function(result) {
     console.log(result)
   })
   .fail(function(err) {
     console.log("Failed")
   });
-  
-  Template.body.helpers({
-    tasks: function() {
-      if (Session.get("hideCompleted")) {
-        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      }
-      else {
-        return Tasks.find({}, {sort:{createdAt: -1}});
-      }
-    }, 
-    hideCompleted: function() {
-      return Session.get("hideCompleted");
-    },
-    incompleteCount: function() {
-      return Tasks.find({checked: {$ne: true}}).count();
-    }
-  });
-  
-  Template.body.events({    
-    // Submit event for new task form
-    "submit .new-task": function (event) {
-      var text = event.target.text.value;
-      
-      // Add new task to list
-      Tasks.insert({
-        text: text,
-        createdAt: new Date()
-      });
-      
-      console.log(event)
-      
-      // Clear new task form
-      event.target.text.value = "";
-      
-      // Prevent default form submit
-      return false;
-    },
-    "change .hide-completed input": function (event) {
-      Session.set("hideCompleted", event.target.checked);
-    }
-  });
-  
-  Template.task.events({
-    // Toggle task checkbox event
-    "click .toggle-checked": function() {
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
-    },
-    // Delete task button event
-    "click .delete": function() {
-      Tasks.remove(this._id)
-    }
-  });
-}
