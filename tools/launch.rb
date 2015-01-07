f1c0e9e0cf06999b3afce49b6da6a4a59149c09d
devion-idle <devion.idle@gmail.com>
2015-01-06 22:09:13 -0500

    Copy new files as part of deploy instead of launch

--- tools/launch.rb
+++ tools/launch.rb
@@ -1,9 +1,5 @@
 require 'fileutils'
 
-Dir.chdir("/var/www/")
-FileUtils.rm_rf("stepkingdom.com")
-FileUtils.mv("bundle", "stepkingdom.com")
-
-Dir.chdir("stepkingdom.com")
+Dir.chdir("/var/www/stepkingdom.com")
 system("(cd programs/server/ && npm install)
 PORT=3000 MONGO_URL=mongodb://localhost:27017/stepkingdom ROOT_URL=https://www.stepkingdom.com node main.js")
