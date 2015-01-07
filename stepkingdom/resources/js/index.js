8f16f07b873ad977dba3cbe804bd0b2c496ae68f
devion-idle <devion.idle@gmail.com>
2015-01-06 22:02:41 -0500

    Restructure project

---
+++ stepkingdom/resources/js/index.js
@@ -0,0 +1,24 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find({});
+    }
+  });
+  
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+      
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      
+      event.target.text.value = "";
+      
+      return false;
+    }
+  });
+}
